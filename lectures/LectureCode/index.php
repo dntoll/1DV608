85136e1886c9f484b097993592ee3ab91fbf5543
dntoll <daniel@familjentoll.se>
2015-09-15 09:45:59 +0200

    save

--- lectures/LectureCode/index.php
+++ lectures/LectureCode/index.php
@@ -23,6 +23,7 @@ $productCatalog = new \model\ProductCatalog();
 
 //Some temporary code to get stuff up and running
 $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum efficitur vel quam eget sollicitudin. Nunc sed libero felis. Praesent a lacus quis ex semper sollicitudin. Aenean id urna at leo semper tincidunt. Nunc at tortor erat. Fusce scelerisque est dolor, non aliquam nunc tempus in. Mauris pellentesque, magna sed fringilla pellentesque, eros magna euismod sapien, eu vehicula ligula sem ut mi. Donec porttitor tempor ex ut aliquam. Aliquam luctus blandit sapien in fringilla. Donec pharetra mauris eu dui semper, a rutrum nunc mattis. Aliquam porta odio eget nulla posuere tempus. Integer pharetra felis nec ante commodo bibendum. Ut vehicula metus sed ligula condimentum, sit amet mollis ante rhoncus.";
+
 $product1 = new \model\Product("Sony 50\"  SuperWide Field", 15000, "sony_50", $description);
 $product2 = new \model\Product("Philips 22\"  Super fat", 150, "philips_22_fat", $description);
 
@@ -37,4 +38,4 @@ $store->doStore();
 //Generate output
 $view = $store->getView();
 $htmlView = new \view\HTMLView("utf-8");
-echo $htmlView->getHTMLPage("I like it!", $view->getHTML());
+echo $htmlView->getHTMLPage("I like it!", $view->getHTML() ."<img src='http://yuml.me/57f022cc'/>");
