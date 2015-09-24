f03a32001f545d37b6b9ed7b11f523aeebbee2eb
dntoll <daniel@familjentoll.se>
2015-09-24 11:08:02 +0200

    added solution for assignment 2

---
+++ Assignments/Assignment_2/solution/model/UserClient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace model;
+
+class UserClient {
+	private $remoteAddr;
+	private $userAgent;
+
+	public function __construct($remoteAddr, $userAgent) {
+		$this->remoteAddr = $remoteAddr;
+		$this->userAgent = $userAgent;
+	}
+
+	public function isSame(UserClient $other) {
+		return  $other->remoteAddr == $this->remoteAddr && 
+				$other->userAgent == $this->userAgent;
+	}
+}
